2cf92ca2d3d6f3cdbf5f260f086573bf9cf1f000
Erikwira <dashfly136@gmail.com>
2024-12-13 22:55:20 +0700

    tugas 1

--- database/poliklinik-bk.sql
+++ database/poliklinik-bk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 09 Des 2024 pada 02.14
+-- Waktu pembuatan: 13 Des 2024 pada 16.53
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -26,64 +26,344 @@ SET time_zone = "+00:00";
 --
 -- Struktur dari tabel `daftar_poli`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.daftar_poli: #1932 - Table &#039;poliklinik-bk.daftar_poli&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.daftar_poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`daftar_poli`&#039; at line 1
+
+CREATE TABLE `daftar_poli` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_pasien` int(11) UNSIGNED NOT NULL,
+  `id_jadwal` int(11) UNSIGNED NOT NULL,
+  `keluhan` text NOT NULL,
+  `no_antrian` int(10) UNSIGNED NOT NULL,
+  `status_periksa` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `daftar_poli`
+--
+
+INSERT INTO `daftar_poli` (`id`, `id_pasien`, `id_jadwal`, `keluhan`, `no_antrian`, `status_periksa`) VALUES
+(3, 10, 8, 'Sakit tenggorokan perih\r\n', 1, '1'),
+(4, 13, 11, 'Gusi berdarah', 1, '0'),
+(5, 10, 8, 'Telinga sakit', 2, '0'),
+(6, 14, 13, 'Mata Merah', 1, '1');
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `detail_periksa`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.detail_periksa: #1932 - Table &#039;poliklinik-bk.detail_periksa&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.detail_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`detail_periksa`&#039; at line 1
+
+CREATE TABLE `detail_periksa` (
+  `id` int(11) NOT NULL,
+  `id_periksa` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `detail_periksa`
+--
+
+INSERT INTO `detail_periksa` (`id`, `id_periksa`, `id_obat`) VALUES
+(4, 3, 13),
+(5, 3, 14),
+(6, 4, 19);
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `dokter`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.dokter: #1932 - Table &#039;poliklinik-bk.dokter&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.dokter: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`dokter`&#039; at line 1
+
+CREATE TABLE `dokter` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_hp` varchar(50) NOT NULL,
+  `id_poli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `dokter`
+--
+
+INSERT INTO `dokter` (`id`, `nama`, `alamat`, `no_hp`, `id_poli`) VALUES
+(12, 'Dr. Kusuma Wardana', 'Jl. Terus No.5 RT10/01', '085678876543', 11),
+(13, 'Dr. Andra', 'Jl. Ramai sekali', '08907524675678', 21),
+(16, 'Novan', 'Jl. Raya No.7', '089456543456', 11),
+(19, 'budi', 'Jl. Simpang 5', '089234567', 22);
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `jadwal_periksa`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.jadwal_periksa: #1932 - Table &#039;poliklinik-bk.jadwal_periksa&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.jadwal_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`jadwal_periksa`&#039; at line 1
+
+CREATE TABLE `jadwal_periksa` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_dokter` int(11) NOT NULL,
+  `hari` enum('Senin','Selasa','Rabu','Kamis','Jumat','Sabtu') NOT NULL,
+  `jam_mulai` time NOT NULL,
+  `jam_selesai` time NOT NULL,
+  `aktif` char(1) NOT NULL DEFAULT 'N'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `jadwal_periksa`
+--
+
+INSERT INTO `jadwal_periksa` (`id`, `id_dokter`, `hari`, `jam_mulai`, `jam_selesai`, `aktif`) VALUES
+(6, 12, 'Senin', '08:00:00', '12:00:00', 'N'),
+(7, 12, 'Selasa', '13:00:00', '17:00:00', 'N'),
+(8, 12, 'Senin', '18:00:00', '22:00:00', 'Y'),
+(9, 13, 'Senin', '13:00:00', '17:00:00', 'Y'),
+(10, 16, 'Senin', '08:00:00', '12:00:00', 'N'),
+(11, 16, 'Selasa', '13:00:00', '17:00:00', 'Y'),
+(12, 19, 'Senin', '08:00:00', '12:30:00', 'N'),
+(13, 19, 'Senin', '13:00:00', '15:30:00', 'Y');
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `obat`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.obat: #1932 - Table &#039;poliklinik-bk.obat&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.obat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`obat`&#039; at line 1
+
+CREATE TABLE `obat` (
+  `id` int(11) NOT NULL,
+  `nama_obat` varchar(50) NOT NULL,
+  `kemasan` varchar(35) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
+(11, 'Obat Batuk', 'Kaplet', 18000),
+(12, 'Obat Pusing', 'botol', 32000),
+(13, 'Vitamin C', 'Botol Pil', 10000),
+(14, 'Vitamin B Complex', 'Botol Pil', 10000),
+(17, 'Obat Radang', 'Kaplet', 23000),
+(18, 'Obat Diare', 'Kaplet', 12000),
+(19, 'Vitamin A', 'Tablet', 8000);
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `pasien`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.pasien: #1932 - Table &#039;poliklinik-bk.pasien&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.pasien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`pasien`&#039; at line 1
+
+CREATE TABLE `pasien` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `no_ktp` varchar(255) NOT NULL,
+  `no_hp` varchar(50) NOT NULL,
+  `no_rm` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`id`, `nama`, `alamat`, `no_ktp`, `no_hp`, `no_rm`) VALUES
+(10, 'Andra', 'Semarang', '123', '085', '202401-001'),
+(13, 'Budi', 'Semarang', '0987654', '98765', '202401-002'),
+(14, 'Dian', 'Semarang', '9876543', '0878', '202401-003'),
+(15, 'danito', 'Semarang', '337414140123456', '089444555666', '202412-004'),
+(16, 'Erik', 'Semarang', '37374140190720002', '0895325106030', '202412-005'),
+(17, 'Putri', 'Jatisari', '3737414081120001', '089555333444', '202412-006');
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `periksa`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.periksa: #1932 - Table &#039;poliklinik-bk.periksa&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`periksa`&#039; at line 1
+
+CREATE TABLE `periksa` (
+  `id` int(11) NOT NULL,
+  `id_daftar_poli` int(11) UNSIGNED NOT NULL,
+  `tgl_periksa` datetime NOT NULL,
+  `catatan` text NOT NULL,
+  `biaya_periksa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `periksa`
+--
+
+INSERT INTO `periksa` (`id`, `id_daftar_poli`, `tgl_periksa`, `catatan`, `biaya_periksa`) VALUES
+(3, 3, '2024-01-08 01:07:00', 'silahkan tidur cukup', 170000),
+(4, 6, '2024-01-08 14:15:00', 'sudah sedikit membaik, jangan lupa habiskan obatnya', 158000);
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `poli`
 --
--- Kesalahan membaca struktur untuk tabel poliklinik-bk.poli: #1932 - Table &#039;poliklinik-bk.poli&#039; doesn&#039;t exist in engine
--- Kesalahan membaca data untuk tabel poliklinik-bk.poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `poliklinik-bk`.`poli`&#039; at line 1
+
+CREATE TABLE `poli` (
+  `id` int(11) NOT NULL,
+  `nama_poli` varchar(25) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `poli`
+--
+
+INSERT INTO `poli` (`id`, `nama_poli`, `keterangan`) VALUES
+(10, 'Poli THT', 'Available'),
+(11, 'Poli Gigi', 'Available'),
+(21, 'Poli Gizi', 'kosong'),
+(22, 'Poli Mata Baik', 'kosong'),
+(23, 'Poli Syaraf', 'Available ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_daftarPoli_jadwal` (`id_jadwal`),
+  ADD KEY `fk_daftarPoli_pasien` (`id_pasien`);
+
+--
+-- Indeks untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_detailPeriksa_periksa` (`id_periksa`),
+  ADD KEY `fk_detailPeriksa_obat` (`id_obat`);
+
+--
+-- Indeks untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_dokter_poli` (`id_poli`);
+
+--
+-- Indeks untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_jadwal_dokter` (`id_dokter`);
+
+--
+-- Indeks untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_periksa_daftarPoli` (`id_daftar_poli`);
+
+--
+-- Indeks untuk tabel `poli`
+--
+ALTER TABLE `poli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `obat`
+--
+ALTER TABLE `obat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT untuk tabel `pasien`
+--
+ALTER TABLE `pasien`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `poli`
+--
+ALTER TABLE `poli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `daftar_poli`
+--
+ALTER TABLE `daftar_poli`
+  ADD CONSTRAINT `fk_daftarPoli_jadwal` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksa` (`id`),
+  ADD CONSTRAINT `fk_daftarPoli_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `detail_periksa`
+--
+ALTER TABLE `detail_periksa`
+  ADD CONSTRAINT `fk_detailPeriksa_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_detailPeriksa_periksa` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `dokter`
+--
+ALTER TABLE `dokter`
+  ADD CONSTRAINT `fk_dokter_poli` FOREIGN KEY (`id_poli`) REFERENCES `poli` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `jadwal_periksa`
+--
+ALTER TABLE `jadwal_periksa`
+  ADD CONSTRAINT `fk_jadwal_dokter` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `periksa`
+--
+ALTER TABLE `periksa`
+  ADD CONSTRAINT `fk_periksa_daftarPoli` FOREIGN KEY (`id_daftar_poli`) REFERENCES `daftar_poli` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
